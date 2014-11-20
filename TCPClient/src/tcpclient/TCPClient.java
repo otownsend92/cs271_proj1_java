0186466d5714b73d6b2c1bb991cca9a5185776e2
Wei Dai <wdai@novacoast.com>
2014-11-20 14:58:25 -0800

    Multhread server @ clientServer functional

--- TCPClient/src/tcpclient/TCPClient.java
+++ TCPClient/src/tcpclient/TCPClient.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tcpclient;
 
 import java.io.*;
@@ -21,7 +16,7 @@ public class TCPClient {
         String sentence;
         String modifiedSentence;
         BufferedReader inFromUser = new BufferedReader( new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("localhost", 12000);
+        Socket clientSocket = new Socket("localhost", 12002);
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         sentence = inFromUser.readLine();
--- clientServer/src/clientserver/ClientServer.java
+++ clientServer/src/clientserver/ClientServer.java
@@ -1,22 +1,42 @@
 package clientserver;
 
 import java.io.IOException;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.*;
-import java.net.*;
 
-public class ClientServer implements Runnable {
-      
+public class ClientServer implements Runnable {   
+    
     String clientSentence;
     String capitalizedSentence;
-    
-    ServerSocket welcomeSocket = new ServerSocket(12000);    
-    System.out.println("waiting for clients...");
-    
-    public static void main(String[] args) {
-              
+    Socket csocket;
+    ClientServer(Socket csocket) {
+        this.csocket = csocket;
+    }
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket welcomeSocket = new ServerSocket(12002);    
+        System.out.println("waiting for clients...");
+        while (true) {
+            Socket connectionSocket = welcomeSocket.accept();
+            System.out.println("Connected");
+            new Thread(new ClientServer(connectionSocket)).start();
+      }
     }
     
+    public void run() {
+        try {
+            BufferedReader inFromClient =
+            new BufferedReader(new InputStreamReader(csocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(csocket.getOutputStream());
+            clientSentence = inFromClient.readLine();
+            System.out.println("Received: " + clientSentence);
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            outToClient.writeBytes(capitalizedSentence);
+        }
+        
+        catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 }
