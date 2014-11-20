9111feb26f5a4e8d7a16af5596f8a25fa7de0801
Oliver Townsend <otownsend92@gmail.com>
2014-11-20 15:12:54 -0800

    added some command line functionality to main

--- clientServer/src/clientserver/ClientServer.java
+++ clientServer/src/clientserver/ClientServer.java
@@ -7,9 +7,18 @@ import java.io.*;
 
 public class ClientServer implements Runnable {   
     
+    private static int serverID;
+    private static int roundn;
+    private static int procn;
+    private static int leader = serverID;   // assigning self to leader
+    private static double balance = 0.0;
+    private static int[] serverPorts = {12000, 12001, 12002, 12003, 12004};
+    
     String clientSentence;
     String capitalizedSentence;
     Socket csocket;
+    
+    
     ClientServer(Socket csocket) {
         this.csocket = csocket;
     }
@@ -17,11 +26,38 @@ public class ClientServer implements Runnable {
     public static void main(String[] args) throws Exception {
         ServerSocket welcomeSocket = new ServerSocket(12002);    
         System.out.println("waiting for clients...");
-        while (true) {
-            Socket connectionSocket = welcomeSocket.accept();
-            System.out.println("Connected");
-            new Thread(new ClientServer(connectionSocket)).start();
-      }
+        String inputLine = null;
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        
+        while(true) {
+            System.out.print("> ");
+            try {
+                inputLine = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String[] input = inputLine.split("\\s+");
+            
+            if(input[0].equals("deposit")) {
+                System.out.println("Depositing: " + input[1]);
+                balance += Double.parseDouble(input[1]);
+            }
+            
+            else if(input[0].equals("withdraw")) {
+                System.out.println("Withdrawing: " + input[1]);
+                balance -= Double.parseDouble(input[1]);
+            }
+            
+            else if(input[0].equals("balance")) {
+                System.out.println("Balance is: " + balance);
+            }
+            
+            else if(input[0].equals("quit")) {
+                System.out.println("Quitting...");
+                System.exit(0);
+            }
+        }
     }
     
     public void run() {
@@ -39,4 +75,18 @@ public class ClientServer implements Runnable {
             System.out.println(e);
         }
     }
+    
+    
+    public void sendToLeader(String m) throws Exception {
+        
+        Socket clientSocket = new Socket("localhost", serverPorts[leader]);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        outToServer.writeBytes(m);
+        //modifiedSentence = inFromServer.readLine();
+        //System.out.println("FROM SERVER: " + modifiedSentence);
+        clientSocket.close();
+    }
+    
+    
 }
